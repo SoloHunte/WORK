f953861e8efd730250895f6257e314f86ce460cc
@kamyshev_pavel <psol@yandex.ru>
2022-05-06 23:00:10 +0300

    просран сбор коэф-в

--- betboom_start.py
+++ betboom_start.py
@@ -25,148 +25,13 @@ headers = {
 }
 
 
-def score(gameInfo['SS'], sport_id):
-    # SCORE
-    try:
-        # Футбол, Киберфутбол, Футзал, Гандбол
-        if (sport_id in ['1', '7', '14', '15']):
-            if (time_game != ''):
-                time_game = str(int(time_game) // 60)
-            else:
-                time_game = '0'
-            if ('ИТОГ' not in state):
-                if ('(' in state):
-                    dop_score = state.split('(')
-                    dop_score = dop_score[1].split(')')
-                    dop_score = dop_score[0].strip()
-                    dop_score = dop_score.replace('-', ':')
-                    sc1 = int(dop_score.split(':')[0])
-                    sc2 = int(dop_score.split(':')[1])
-                    score1 = int(score.split(':')[0])
-                    score2 = int(score.split(':')[1])
-                    dop_score = dop_score + ', ' + str(score1 - sc1) + ':' + str(score2 - sc2)
-                else:
-                    dop_score = score
-            else:
-                dop_score = state.replace(' ИТОГ', '')
-                dop_score = dop_score.replace('-', ':')
-                arrD = dop_score.split(' ')
-                dop_score = dop_score.replace(' ', ', ')
-                if (len(arrD) > 1):
-                    if ('(' in state):
-                        dop_score = state.split('(')
-                        dop_score = dop_score[1].split(')')
-                        dop_score = dop_score[0].strip()
-                        dop_score = dop_score.replace('-', ':')
-                        try:
-                            sc1 = int(dop_score.split(':')[0])
-                            sc2 = int(dop_score.split(':')[1])
-                            score1 = int(score.split(':')[0])
-                            score2 = int(score.split(':')[1])
-                            dop_score = dop_score + ', ' + str(score1 - sc1) + ':' + str(score2 - sc2)
-                        except:
-                            dop_score = dop_score.replace(' ', ', ')
-                    else:
-                        dop_score = score
-                else:
-                    dop_score = dop_score.replace('(', ' ')
-                    dop_score = dop_score.replace(')', ' ')
-            score = score + ' (' + dop_score + ')'
-        # Теннис
-        if (sport_id == '2'):
-            time_game = 'None'
-            if (score == ''):
-                score = '0:0'
-            state = state.replace('-', ':')
-            state = state.strip()
-            state = state.replace(' ', ', ')
-            state = state.replace('),', ')')
-            score = score + ' ' + state
-        # Настольный теннис, Волейбол, Бадминтон, Керлинг
-        if (sport_id in ['3', '5', '11', '16']):
-            time_game = 'None'
-            if (score == ''):
-                score = '0:0'
-            if ('(' in state):
-                state = state.split('(')
-                state = state[1].split(')')
-                state = state[0].strip()
-                state = '(' + state.replace('-', ':') + ')'
-                state = state.replace(' ', ', ')
-                score = score + ' ' + state
-                score = score.replace('*', '')
-            else:
-                score = score + ' (' + score + ')'
-        # Баскетбол
-        if (sport_id in ['4', '9', '12']):
-            if (time_game != ''):
-                time_game = str(int(time_game) // 60)
-                if '0' in time_game:
-                    time_game = 'Break'
-            else:
-                time_game = '0'
-            if ('(' in state):
-                state = state.split('(')
-                state = state[1].split(')')
-                state = state[0].strip()
-                state = '(' + state.replace('-', ':') + ')'
-                state = state.replace(' ', ', ')
-                score = score + ' ' + state
-            else:
-                score = score + ' (' + score + ')'
-        # Хоккей
-        if (sport_id in ['6', '10']):
-            if (time_game != ''):
-                time_game = str(int(time_game) // 60)
-                if '0' in time_game:
-                    time_game = 'Break'
-            else:
-                time_game = '0'
-            if ('(' in state):
-                dop_score = state.split('(')
-                dop_score = dop_score[1].split(')')
-                dop_score = dop_score[0].strip()
-                dop_score = dop_score.replace('-', ':')
-                arrSt = dop_score.split(' ')
-                score1 = int(score.split(':')[0])
-                score2 = int(score.split(':')[1])
-                for iR in arrSt:
-                    sc1 = int(iR.split(':')[0])
-                    sc2 = int(iR.split(':')[1])
-                    score1 -= sc1
-                    score2 -= sc2
-                dop_score = dop_score + ' ' + str(score1) + ':' + str(score2)
-                dop_score = dop_score.replace(' ', ', ')
-                score = score + ' (' + dop_score + ')'
-            else:
-                score = score + ' (' + score + ')'
-        # Киберспорт
-        if (sport_id == '8'):
-            time_game = 'None'
-            if ('(' in state):
-                state = state.split('(')
-                state = state[1].split(')')
-                state = state[0].strip()
-                state = '(' + state.replace('-', ':') + ')'
-                state = state.replace(' ', ', ')
-                score = score + ' ' + state
-                score = score.replace('*', '')
-            else:
-                score = score + ' (' + score + ')'
+def right_score(ss, hos, aws, num):
+    if int(num) > 1:
+        ss = ", ".join(ss.split(',')[:int(num)])
+    else:
+        ss = ", ".join(ss.split(',')[:1])
 
-        if ('Break' in state) or ('Таймаут' in state):
-            time_game = 'Break'
-        if ('Match has not started' in state) or ('Матч не начался' in state):
-            time_game = 'Not start'
-        if ('END' in state) or ('FINISHED' in state) or ('ИТОГ' in state):
-            time_game = 'Closed'
-    except:
-        print(sport_id)
-        print(score)
-        print(state)
-        print(time_game)
-        print(traceback.format_exc())
-    # END_SCORE
+    return f'{hos}:{aws} ({ss})'
 
 
 def period(score, sport):
@@ -224,24 +89,23 @@ while skill.connect():
     time.sleep(60)
 cur = skill.cur
 conn = skill.conn
-timeout = 30
+timeout = 35
 bk_name = 'bet_boom'
 
 voc_sports = {1: '1', 3: '2', 15: '3', 4: '4', 12: '5', 10: '6', 94: '7', 53: '8', 95: '9', 96: '10', 17: '14',
-              13: '15'}
+              13: '15', 25: '3'}
 # session = requests.Session()
 try:
     while True:
         skill.start_session()
         link = 'https://sport.betboom.ru/Live/Sports?langId=1&partnerId=147&countryCode=RU'
-
         data = skill.scaner_data_json(link, headers, timeout)
         kolGame = 0
         sportActive = {}
         info_liga = {}
         info_gamer = {}
         info_koef = {}
-        name_kof_skill = {}
+        name_koef = {}
         koef_skill = {}
 
         for voc in data:
@@ -273,7 +137,12 @@ try:
                                     gamer1 = gameInfo['HT']
                                     gamer2 = gameInfo['AT']
                                     game_id = gameInfo['Id']
-                                    periodName = period(gameInfo['ES'], voc_sports[sportIdThis])
+                                    print(f'Номер спорта {sportIdThis}')
+                                    try:
+                                        periodName = period(gameInfo['ES'], voc_sports[sportIdThis])
+                                    except Exception:
+                                        print('не нужный спорт')
+                                        continue
                                     # started_at = gameInfo['D']
                                     id_game_hash = hashlib.md5(
                                         (str(gameInfo['Id']) + bk_name).encode('utf-8')).hexdigest()
@@ -303,7 +172,11 @@ try:
                                 time_game = gameInfo['PT']
                                 started_at = str(datetime.datetime.now())
                                 sport_id = sportid_my
-                                score = score(gameInfo['SS'], sportid_my)
+                                num_period = str(gameInfo['CP'])
+                                scoreSS = str(gameInfo['SS']).replace('-', ',').replace(' ', '')
+                                scoreHS = str(gameInfo['HS'])
+                                scoreAS = str(gameInfo['AS'])
+                                score = right_score(scoreSS, scoreHS, scoreAS, num_period)
 
                                 active_sport_name = skill_scaner.active_sport_names(bk_name)
                                 info_liga[id_liga_hash] = {'ligaName': ligaName, 'sport_Id': sportid_my,
